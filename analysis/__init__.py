f35c7362a43dd3232850eb9225a05a468211525d
user-name1729 <2758547325@qq.com>
2026-07-19 21:11:50 +0800

    Create analysis module

---
+++ analysis/__init__.py
@@ -0,0 +1,13 @@
+"""
+Analysis Module
+
+Performance metrics, aberration analysis, and optimization tools.
+"""
+
+__version__ = "0.1.0"
+
+from .metrics import PerformanceMetrics
+
+__all__ = [
+    "PerformanceMetrics",
+]
